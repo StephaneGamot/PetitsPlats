57f81c165a61af1030c3edda728eb168109cca18
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2023-04-11 18:50:00 +0200

    ingredients doublons off

--- JS/Components/ingredients.js
+++ JS/Components/ingredients.js
@@ -1,35 +1,40 @@
 import { recipes } from "../Data/recipes.js";
 
+function isPlural(ingredient) {
+  return ingredient.endsWith('s') && recipes.some(recipe => {
+    return recipe.ingredients.some(({ ingredient: ing }) => ing.toLowerCase() === ingredient.slice(0, -1).toLowerCase());
+  });
+}
+
 export function getAllIngredients() {
-	const allIngredients = new Set();
-	recipes.forEach((recipe) => {
-		recipe.ingredients.forEach((ingredient) => {
-			allIngredients.add(ingredient.ingredient);
-		});
-	});
-	return Array.from(allIngredients);
+  const allIngredients = new Set();
+  recipes.forEach((recipe) => {
+    recipe.ingredients.forEach((ingredient) => {
+      const lowerCaseIngredient = ingredient.ingredient.toLowerCase();
+      if (!isPlural(lowerCaseIngredient)) {
+        allIngredients.add(lowerCaseIngredient);
+      }
+    });
+  });
+  return Array.from(allIngredients);
+}
+
+function sortIngredientsAlphabetically(ingredientsList) {
+  return ingredientsList.sort((a, b) => a.localeCompare(b));
+}
+
+function formatIngredient(ingredient) {
+  return ingredient.charAt(0).toUpperCase() + ingredient.slice(1).toLowerCase();
 }
 
 export function displayIngredients(ingredients) {
-	const ulElement = document.getElementById("xxx");
+  const ulElement = document.getElementById("xxx");
 
-	ingredients.forEach((ingredient) => {
-		const liElement = document.createElement("li");
-		liElement.textContent = ingredient;
-		ulElement.appendChild(liElement);
-	});
+  const sortedIngredients = sortIngredientsAlphabetically(ingredients);
+
+  sortedIngredients.forEach((ingredient) => {
+    const liElement = document.createElement("li");
+    liElement.textContent = formatIngredient(ingredient);
+    ulElement.appendChild(liElement);
+  });
 }
-
-/*
-export function displayIngredients(ingredients) {
-   // Sélectionnez l'élément <main> de la page
-   const mainElement = document.querySelector("main");
-	 const ulElement = document.createElement("ul");
-
-	ingredients.forEach((ingredient) => {
-		const liElement = document.createElement("li");
-		liElement.textContent = ingredient;
-		ulElement.appendChild(liElement);
-	});
-}
-*/
