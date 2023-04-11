03e35cb0b4e28367b43b4bd9040be75a3df35779
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2023-04-11 12:06:56 +0200

    liste ingredient s'affiche

--- JS/script.js
+++ JS/script.js
@@ -4,41 +4,37 @@ import createCard from "./Template/createCard.js";
 import searchAndFilter from "./Components/searchAndFilter.js";
 import displayRecipes from "./Components/displayRecipes.js";
 import { getAllIngredients, displayIngredients } from "./Components/ingredients.js";
+/*
+const label = document.getElementById("allIngredients");
+const span = document.getElementById("ingredientsLabel");
+const input = document.getElementById("ingredient");
+*/
+const ingredientsLabel = document.getElementById("ingredientsLabel");
+const ingredientsSpan = document.getElementById("ingredientsSpan");
+const ingredientInput = document.getElementById("ingredientInput");
 
 document.addEventListener("DOMContentLoaded", function () {
-
 	searchAndFilter(recipes);
 	displayRecipes(recipes, createCard);
-});
 
-document.addEventListener("DOMContentLoaded", function() {
-    const labels = document.querySelectorAll(".search-group > label");
-
-    labels.forEach(label => {
+	const labels = document.querySelectorAll(".search-group > label");
+	labels.forEach((label) => {
 		label.addEventListener("click", () => {
 			const span = label.querySelector("span");
-      const input = label.querySelector("input");
-      const icon = label.querySelector("i");
+			const input = label.querySelector("input");
+			const icon = label.querySelector("i");
+      const ingredients = getAllIngredients();
+      displayIngredients(ingredients);
 			span.style.display = "none";
-			input.style.display = "block";
-		
+			input.style.display = "flex";
+
 			span.classList.toggle("hidden");
-      input.classList.toggle("hidden");
+			input.classList.toggle("hidden");
+
+      ingredientsLabel.style.display = "block";
+
 
 			icon.classList.add("rotate-chevron");
-			
 		});
-    });
-  });
-
-  const label = document.getElementById("allIngredients");
-  const span = document.getElementById("ingredientsLabel");
-  const input = document.getElementById("ingredient");
-
-
-
-  document.addEventListener("DOMContentLoaded", function() {
-	const ingredients = getAllIngredients();
-	displayIngredients(ingredients);
-  });
-  
+	});
+});
